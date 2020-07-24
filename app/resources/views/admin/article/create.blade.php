a8a1ca6975fee86163c6014549c7b704f78dbab1
kamotetu <lewd_magnifico.m@icloud.com>
2020-07-25 01:29:55 +0900

    ひとまず

--- app/resources/views/admin/article/create.blade.php
+++ app/resources/views/admin/article/create.blade.php
@@ -1,1 +1,48 @@
-hello
+@extends('layouts.app')
+@section('headLine')
+    投稿する
+@endsection
+@section('headLineUrl')
+    {{ route('article.create') }}
+@endsection
+@section('content')
+    <div class="article_create_container">
+        <div class="article_create_left_area">
+            <form action="">
+                {{-- @csrf --}}
+            <div class="article_create_title_area">
+                <div class="article_create_title_headline">
+                    <p>タイトル</p>
+                </div>
+                <div class="article_create_title_input">
+                    <input type="text" class="article_create_title_input_form">
+                </div>
+            </div>
+            <div class="article_create_genre_area">
+                <div class="article_create_genre_headline">
+                    <p>ジャンル</p>
+                </div>
+                <div class="article_create_genre_input">
+                    <input type="text">
+                </div>
+            </div>
+            <div class="article_create_tag_area">
+                <div class="article_create_tag_headline">
+                    <p>タグ</p>
+                </div>
+                <div class="article_create_tag_input">
+                    <input type="text" class="article_create_tag_input_form">
+                </div>
+            </div>
+            <div class="article_create_article_area">
+                <div class="article_create_article_headline">
+                    <p>記事</p>
+                </div>
+                <div class="article_create_article_input">
+                    <textarea name="" rows="10" id="article_create_article_input_form"></textarea>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/article_textarea.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -23,8 +24,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                {{-- <a class="navbar-brand" href="{{ url('/') }}"> --}}
+                <a class="navbar-brand" href="@yield('headLineUrl')">
+                    {{-- {{ config('app.name', 'Laravel') }} --}}
+                    @yield('headLine')
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
